1fbe555eeec81c04f324f6760d835d5796b5212b
Ali Shalabi <alishalabi@Alis-MacBook-Pro.local>
2018-08-29 17:11:33 -0700

    Adding index file with first lines of code

---
+++ index.py
@@ -0,0 +1,1 @@
+print("Hello - welome to a game of Mad Libs! In this game, you will input a series of nouns, verbs, and adjectives - and the game will create a customized, hilarious short story for you.")
